d01583ad47678afcfa879881354b21f92cf3cd53
anhluunguyen3113 <nguye713@purdue.edu>
2022-01-22 17:47:52 -0500

    commit 3

--- Hackathon2022/Hackathon2022/urls.py
+++ Hackathon2022/Hackathon2022/urls.py
@@ -1,4 +1,4 @@
-"""HackathonProject1 URL Configuration
+"""Hackathon2022 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
