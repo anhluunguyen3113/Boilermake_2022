1a372872628349f0813c9b14c54cd4e69b7d64f1
anhluunguyen3113 <nguye713@purdue.edu>
2022-01-22 18:44:11 -0500

    commit 4

--- Hackathon2022/authentication/views.py
+++ Hackathon2022/authentication/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
-from ..Hackathon2022 import settings
 
 
 # Create your views here.
@@ -14,7 +13,9 @@ def homes(request):
 
 
 def signup(request):
+    print('Working')
     if request.method == 'POST':
+        print('working2')
         username = request.POST['username']
         fname = request.POST['fname']
         lname = request.POST['lname']
@@ -22,15 +23,11 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-
-        if User.objects.filter(username =username):
+        if User.objects.filter(username=username):
             messages.error(request, "Username taken, please input another username")
             return redirect('home')
 
-        if User.objects.filter(email =email):
+        if User.objects.filter(email=email):
             messages.error(request, "Email already registered")
             return redirect('home')
 
@@ -44,19 +41,23 @@ def signup(request):
             messages.error(request, "Username must be alphanumeric")
             return redirect('home')
 
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
         myuser.save()
 
         messages.success(request, 'Your account has been successfully created')
 
-        #Welcome email
+        # Welcome email
 
         subject = 'Welcome to boilermake - Django login'
         message = 'Hello ' + myuser.first_name + '!!\n' + 'Welcome to boilermake2022!\n' + \
                   'Thank you for visiting our website \n We have also sent you a confirmation email\n ' \
                   'please confirm your email\n Thank you!'
-        from_email = settings.EMAIL_HOST_USER
+        # from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        send_mail(subject, message, to_list, fail_silently=True)
 
         return redirect('signin')
 
@@ -64,7 +65,6 @@ def signup(request):
 
 
 def signin(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         pass1 = request.POST['pass1']
